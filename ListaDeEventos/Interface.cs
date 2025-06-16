ee18bbaf52c1232bc8c64fd7bf9527524d67da92
Luiz Fernando <95312738+hayato2303@users.noreply.github.com>
2025-06-15 23:03:19 -0300

    Add files via upload

---
+++ ListaDeEventos/Interface.cs
@@ -0,0 +1,15 @@
+namespace ListaDeEventos
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Interface_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
